2886ffa552fb66075a3ed2efa5c5b8330a17a809
Alex Casquete <Alex Casquete>
2016-01-27 20:08:18 +0100

    ddd

--- site/webserver.fsx
+++ site/webserver.fsx
@@ -19,39 +19,7 @@ open Suave.DotLiquid
 //let app = OK "Hello world!"
 // ----------------------------------------------------------------------------
 
-// DEMO: Define the domain model
-type Movies =
-  { Id: int
-    Poster : string
-    Title : string
-    VoteAvg : float
-    ReleaseDate : System.DateTime
-  }
 
-type Home = { Movies: seq<Movies> }
-
-
-// TODO: Get current news from BBC
-// (http://feeds.bbci.co.uk/news/rss.xml)
-type Forecast = JsonProvider<"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f0daa403a993eb9b7ee555b903aae2fd">
-
-let posterUrl s =
-    "http://image.tmdb.org/t/p/w185" + s
-
-let getMovies() = 
-  let res = Forecast.Load("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=f0daa403a993eb9b7ee555b903aae2fd")
-  [ for item in res.Results ->
-      { Id = item.Id
-        Poster = item.PosterPath |> posterUrl
-        Title = item.Title
-        VoteAvg = float item.VoteAverage
-        ReleaseDate = item.ReleaseDate
-         } ] 
-
-
-let app : WebPart = fun ctx -> async {
-  let movies = getMovies()
-  return! DotLiquid.page "index.html" { Movies = movies } ctx }
   
 let serverConfig = 
     let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
