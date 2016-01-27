604daf80f3649e89462b9471c2c60021af4190e8
Alex Casquete <Alex Casquete>
2016-01-27 21:21:27 +0100

    build

--- build.fsx
+++ build.fsx
@@ -3,13 +3,84 @@
 // --------------------------------------------------------------------------------------
 
 #r @"packages/FAKE/tools/FakeLib.dll"
+#r "packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
+#r "packages/Suave/lib/net40/Suave.dll"
 
 open Fake
-
+open System
+open System.IO
+open Suave
+open Suave.Web
+open Suave.Filters
+open Suave.Successful      
+open Suave.Filters
+open Microsoft.FSharp.Compiler.Interactive.Shell
 
 // --------------------------------------------------------------------------------------
-// Run all targets by default. Invoke 'build <Target>' to override
+// The following uses FileSystemWatcher to look for changes in 'app.fsx'. When
+// the file changes, we run `#load "app.fsx"` using the F# Interactive service
+// and then get the `App.app` value (top-level value defined using `let app = ...`).
+// The loaded WebPart is then hosted at localhost:8083.
+// --------------------------------------------------------------------------------------
+let sbOut = new Text.StringBuilder()
+let sbErr = new Text.StringBuilder()
 
-Target "All" DoNothing
+let fsiSession =
+  let inStream = new StringReader("")
+  let outStream = new StringWriter(sbOut)
+  let errStream = new StringWriter(sbErr)
+  let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
+  let argv = Array.append [|"/fake/fsi.exe"; "--quiet"; "--noninteractive"; "-d:DO_NOT_START_SERVER"|] [||]
+  FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream)
 
-RunTargetOrDefault "All"
+let reportFsiError (e:exn) =
+  traceError "Reloading app.fsx script failed."
+  traceError (sprintf "Message: %s\nError: %s" e.Message (sbErr.ToString().Trim()))
+  sbErr.Clear() |> ignore
+
+let reloadScript () =
+  try
+    traceImportant "Reloading app.fsx script..."
+    let appFsx = __SOURCE_DIRECTORY__ @@ "/site/app.fsx"
+    fsiSession.EvalInteraction(sprintf "#load @\"%s\"" appFsx)
+    fsiSession.EvalInteraction("open App")
+    match fsiSession.EvalExpression("app") with
+    | Some app -> Some(app.ReflectionValue :?> WebPart)
+    | None -> failwith "Couldn't get 'app' value"
+  with e -> reportFsiError e; None
+
+// --------------------------------------------------------------------------------------
+// Suave server that redirects all request to currently loaded version
+// --------------------------------------------------------------------------------------
+
+let currentApp = ref (fun _ -> async { return None })
+
+let serverConfig =
+  { defaultConfig with
+      homeFolder = Some __SOURCE_DIRECTORY__
+      logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Debug
+      bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" 8034] }
+
+let reloadAppServer () =
+  reloadScript() |> Option.iter (fun app ->
+    currentApp.Value <- app
+    traceImportant "New version of app.fsx loaded!" )
+
+Target "run" (fun _ ->
+  let app ctx = currentApp.Value ctx
+  let _, server = startWebServerAsync serverConfig app
+
+  // Start Suave to host it on localhost
+  reloadAppServer()
+  Async.Start(server)
+  // Open web browser with the loaded file
+  System.Diagnostics.Process.Start("http://localhost:8034") |> ignore
+
+  // Watch for changes & reload when app.fsx changes
+  use watcher = !! (__SOURCE_DIRECTORY__ @@ "*.*") |> WatchChanges (fun _ -> reloadAppServer())
+  traceImportant "Waiting for app.fsx edits. Press any key to stop."
+  //System.Console.ReadLine() |> ignore
+  System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite)
+)
+
+RunTargetOrDefault "run"
